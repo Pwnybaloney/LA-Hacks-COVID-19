b9f595fce2aad70b52ad029a510ae46c394eea62
Tony Cui <cuitony.baloney@gmail.com>
2020-03-28 18:55:29 -0700

    commentation

--- send_sms.py
+++ send_sms.py
@@ -2,12 +2,12 @@
 from twilio.rest import Client
 
 import os
-# Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
+#Set SID and auth tokens
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
+#sent the message
 message = client.messages \
     .create(
          body="Join Earth's mightiest heroes. Like Kevin Bacon.",
