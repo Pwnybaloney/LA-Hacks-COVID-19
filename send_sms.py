d27755eaf59db027a9f2f3e8e67a7322d1e29554
Tony Cui <cuitony.baloney@gmail.com>
2020-03-28 18:18:32 -0700

    scipt

--- send_sms.py
+++ send_sms.py
@@ -5,14 +5,14 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 account_sid = 'AC3ddd25f3b0751e1402277dbea25ab7b9'
-auth_token = '219c42812324a9d158f31d733a7621f5'
+auth_token = 'c04c20800d88d25f1efc401b3245200b'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
-         body="twilio be wurkin",
+         body="wubbalubbadubdub.",
          messaging_service_sid='MG293888266321446c593037379a6d6a6a',
-         to='+15622159912'
+         to='+15627326959'
      )
 
-
+print(message.sid)
